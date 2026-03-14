09610ead04ed900a171515d335e52b2f5b4bf451
khanh <maisolokhanh999@gmail.com>
2026-03-14 17:33:32 +0700

    01

--- khanh/src/App.tsx
+++ khanh/src/App.tsx
@@ -1,17 +1,77 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const data = [
+  {
+    id: 1,
+    movieName: "Weather With You",
+    description: "Mùa hè năm đầu cấp Ba, Hodaka bỏ nhà ra đi. Cậu từ một hòn đảo xa xôi hẻo lánh đến Tokyo, nhưng sau đó nhanh chóng rơi vào cảnh túng thiếu và phải sống chuỗi ngày cô đơn. Nhưng cuối cùng cậu đã tìm được công việc: viết bài cho một tạp chí huyền bí. Sau khi cậu bắt đầu công việc, mưa cứ rơi mãi rơi mãi không thôi. Ở một góc thành phố đông đúc và nhộn nhịp, Hodaka đã gặp thiếu nữ tên Hina. Cô sống cùng em trai, luôn vui vẻ và kiên cường. Cô cũng có một sức mạnh vô cùng đặc biệt: “Này, từ bây giờ trời hãy hửng nắng đi nào.” Từng chút một, mưa ngừng rơi, và ánh sáng tuyệt đẹp rọi chiếu những nóc nhà trong thành phố. Chỉ bằng một lời cầu nguyện, cô đã khiến bầu trời trở nên sáng trong.",
+    image: "khanh/src/assets/tải xuống.jpg",
+    episode: "01"
+  },
+  {
+    id: 2,
+    movieName: "Once Peace",
+    description: "One Piece là bộ truyện tranh dành cho thiếu niên của tác giả nổi tiếng Oda Eiichiro. Bộ manga này được chuyển thể thành một series anime nhiều tập hài hước, đặc sắc. Phim Đảo Hải Tặc mở đầu câu chuyện bằng cảnh xử tử vua hải tặc Gol D. Roger. Trước khi chết ông đã tiết lộ rằng mình có một kho báu được giấu ngoài biển. Nếu ai tìm thấy kho báu thì chúng sẽ thuộc về người đó. Rất nhiều người gan dạ đã đổ xô ra biển để tìm kiếm kho báu bí ẩn của vua hải tặc Gol D. Roger.",
+    image: "khanh/src/assets/images.jpg",
+    episode: "1018"
+  },
+  {
+    id: 3,
+    movieName: "Boruto: Naruto Next Generations",
+    description: "Boruto: Naruto Next Generations kể về thế hệ sau của Naruto. Bộ phim vẫn lấy bối cảnh là thế giới Ninja nhưng nhân vật chính lần này lại là Uzumaki Boruto, con trai của Uzumaki Naruto và Hyuga Hinata. Đồng hành cùng Naruto trong lần quay lại này là Sarada Uchiha là con gái của Sasuke Uchiha, Sakura Haruno và Mitsuki – “con” của Orochimaru.",
+    image: "khanh\src\assets\images (1).jpg",
+    episode: "250"
+  },
+  {
+    id: 4,
+    movieName: "Spy X Family",
+    description: "Bộ phim là phần phim điện ảnh của series anime nổi tiếng Spy x Family. Trong bộ phim lần này, sau khi nhận được yêu cầu thay thế mình trong Chiến dịch Strix, Loid Forger quyết định giúp con gái Anya chiến thắng trong cuộc thi nấu ăn tại Học viện Eden bằng cách nấu bữa ăn yêu thích của hiệu trưởng để tránh bị thay thế khỏi nhiệm vụ mật. Nhưng từ đây, gia đình Forger phát hiện ra bí mật kinh hoàng ảnh hưởng đến hòa bình thế giới",
+    image: "khanh/src/assets/images (2).jpg",
+    episode: "07"
+  },
+  {
+    id: 5,
+    movieName: "Shingeki no kyojin",
+    description: "Câu chuyện của Đại chiến Titan tập trung vào một nền văn minh bên trong ba bức tường đồ sộ quây tròn đồng tâm, nơi được cho là duy nhất mà nhân loại còn tồn tại. Ba bức tường được đặt tên theo tên ba người con gái của vị vua đầu tiên có tên là Maria, Rose và Sina. Người dân ở đó, được gọi là dân tộc Eldia, đã tin rằng hơn một trăm năm trước, loài người đã bị đẩy đến bờ vực tuyệt chủng sau sự xuất hiện của những người khổng lồ hình dáng con người, được gọi là Titan.",
+    image: "khanh/src/assets/maxresdefault.jpg",
+    episode: "28"
+  },
+  {
+    id: 6,
+    movieName: "Captain Tsubasa",
+    description: "Tsubasa Oozora là nhân vật chính trong bộ truyện này. Khi còn nhỏ, cậu đã thoát chết nhờ một quả bóng bất ngờ. Điều đó đã thúc đẩy cậu trở thành một fan hâm mộ bóng đá. Câu chuyện tập trung vào các trận đấu bóng đá hấp dẫn, đem lại những bất ngờ cho khán giả. Đây là bản làm lại của 'Tsubasa Giấc mơ sân cỏ', với những chi tiết được thêm hoặc bỏ, để cộng thêm nét đặc trưng vào bộ truyện. Bộ phim này đã làm vợi lên kí ức về tuổi thơ của biết bao người.",
+    image: "khanh/src/assets/images (3).jpg",
+    episode: "28"
+  },
+];
+  const [count, setCount] = useState("");
+  const movie = data.find(item => item.id === 1);
   return (
     <>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="px-[84px] py-[15px]">
+        <div className="flex justify-center">
+          <h3 className="flex-1">Anonime</h3>
+          <p className="flex-1">Home</p>
+          <p className="flex-1">List anime</p>
+          <input
+            type="text"
+            placeholder="Search anime or movie"
+            className="w-[400px] h-[34px] rounded-[37px] bg-gray-700 border border-gray-500 px-5 outline-none"
+          />
+        </div>
+        <div className='py-[15px]'>
+          <h4>Explore</h4>
+        </div>
+        <div className='py-[15px]'>
+          <p>What are you gonna watch today ?</p>
+        </div>
+        <div>
+          <img src={movie?.image} alt={movie?.movieName} />
+        </div>
+      </div>
     </>
   )
 }
