7fb99038722a1e7e6439d81ff5432a97abda42a0
khanh <maisolokhanh999@gmail.com>
2026-03-18 10:01:38 +0700

    1

--- khanh1/src/App.tsx
+++ khanh1/src/App.tsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
-import img1 from "./assets/tải xuống.jpg";
-import img2 from "./assets/images.jpg";
-import img3 from "./assets/images (1).jpg";
-import img4 from "./assets/images (2).jpg";
-import img5 from "./assets/maxresdefault.jpg";
-import img6 from "./assets/images (3).jpg";
+import { img1,img2,img3,img4,img5,img6 } from "./assets/img";
+
 
 import './App.css'
 
