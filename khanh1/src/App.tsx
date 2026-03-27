05dd866856a2d99319f41dc8aceb613734facf35
khanh <maisolokhanh999@gmail.com>
2026-03-27 08:47:05 +0700

    1

--- khanh1/src/App.tsx
+++ khanh1/src/App.tsx
@@ -6,8 +6,10 @@ function App() {
   return(
      <div className='app w-screen min-h-screen bg-[#F0F4F5]'>
       <Routes>
-        <Route path='' element={<h1>Hello MindX</h1>} />
-
+        <Route path='/' element={<h1>home</h1>} />
+        <Route path='/services' element={<h1>Services</h1>} />
+        <Route path='/about' element={<h1>About</h1>} />
+        <Route path='/contact' element={<h1>Contact</h1>} />
       </Routes>
     </div>
   )
